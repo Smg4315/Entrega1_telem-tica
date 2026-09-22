f0e36454be4e8fb391701ee35194fe446acbe3e6
Trerass <jeronimocontreras654@gmail.com>
2026-09-22 17:39:55 -0500

    Implement NMP protocol parser and tests

--- server/include/nmp.h
+++ server/include/nmp.h
@@ -1,6 +1,8 @@
 #ifndef NMP_H
 #define NMP_H
 
+#include <stddef.h>
+
 /* Tipos de mensaje NMP (docx, sección 4) */
 typedef enum {
     NMP_REGISTER,
@@ -15,14 +17,13 @@ typedef enum {
 /* Mensaje NMP: TIPO|ID|NODE_ID|DATOS */
 typedef struct {
     nmp_msg_type_t type;
-    unsigned int   id;
-    char           node_id[32];
-    char           data[256];
+    unsigned int    id;
+    char            node_id[32];
+    char            data[256];
 } nmp_message_t;
 
-/* TODO: implementar en src/protocol/nmp_parser.c
-   int nmp_parse(const char *raw, nmp_message_t *out);
-   int nmp_serialize(const nmp_message_t *msg, char *out, size_t out_len);
-*/
+/* Parser del protocolo NMP. Debe aceptar mensajes con payload que puede incluir
+ * separadores | a partir del campo DATOS. */
+int nmp_parse(const char *raw, nmp_message_t *out);
 
 #endif
