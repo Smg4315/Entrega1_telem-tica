56292e82fb1da032829d1a86fa09b65414486b34
ViVi <varangot1@eafit.edu.co>
2026-09-23 13:08:37 -0500

    changes in client_test message

--- server/client_test.py
+++ server/client_test.py
@@ -3,7 +3,7 @@ import socket
 HOST = "127.0.0.1"
 PORT = 8080
 
-message = "INVALID|5|NODE01|TEST\n"
+message = "REGISTER|1|NODE01|NetMonitor\n"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect((HOST, PORT))
